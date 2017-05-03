7e0afb23fefc31907e21d32f829b166b20f57251
sebastiansrc <srcsebastian@gmail.com>
2017-05-03 13:09:15 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -4,7 +4,7 @@ $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 		
 $('#beep').tap(function(){
-navigator.notification.beep(1);
+navigator.notification.beep(2);
 });//tap beep
 
 $('#vibrar').tap(function(){
